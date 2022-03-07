52f95493229c05d01e1eb3c741f013c0df86e632
Dmitry Mikhailov <dm1trym2130@gmail.com>
2022-03-08 01:14:38 +0300

    Fixed layout

--- QLOGA/Intro/IntroView.swift
+++ QLOGA/Intro/IntroView.swift
@@ -16,24 +16,23 @@ struct IntroView: View {
 //    @Binding var addressSearchView: AddressSearchView
     var body: some View {
         NavigationView {
+
                 VStack(alignment: .center, spacing: 10) {
                     AddressSearchBarView
-                    HStack(alignment: .center, spacing: 15) {
+                    HStack(alignment: .center, spacing: 0) {
                         ServicesScrollView.padding([.vertical])
                             .ignoresSafeArea(.all, edges: .bottom).zIndex(1.0)
-//                        Divider().padding(.top).padding(.top, 5)
-                        Spacer()
+
+                        Spacer(minLength: 50)
                         ProfileChooserView
-                    }
+							.frame(width: .infinity, alignment: .trailing).padding(.bottom, 5)//.scaleEffect(1.1, anchor: .topTrailing)
+					}
                 }
-                .padding([.top])
-                .padding(.horizontal, 30)
+                .padding(.horizontal, 20)
+
                 .navigationBarTitle("").navigationBarHidden(true)
         }.environment(\.colorScheme, .light)
-            .sheet(isPresented: $isSearchEnabled) {
-                AddressSearchView(address: $address)
-            }
-           
+   
     }
 }
 
@@ -56,7 +55,7 @@ extension IntroView {
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(address == "Enter new address" || address == "" ? Color.lightGray.opacity(0.6) : Color.init("Orange").opacity(0.8), lineWidth: 1)
-                            .shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
+//                            .shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
                     )
             }
 
@@ -64,7 +63,7 @@ extension IntroView {
 
 
     private var ServicesScrollView: some View {
-        ScrollView(.vertical, showsIndicators: true) {
+        ScrollView(.vertical, showsIndicators: false) {
             ForEach(Services) { service in
                 Button {
                     selectedButton = service.id
@@ -78,10 +77,11 @@ extension IntroView {
                             .overlay(
                                 RoundedRectangle(cornerRadius: 10)
                                     .stroke(selectedButton == service.id ? Color.accentColor : Color.lightGray.opacity(0.6), lineWidth: selectedButton == service.id ? 2.0 : 1.5)
-                                    .shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)).padding(.bottom, -3).padding(.top,2)
+//                                    .shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
+                            ).padding(.bottom, -3).padding(.top,2)
                         Text(service.name).foregroundColor(Color.black)
                             .font(.system(size: 10.0, weight: .light, design: .rounded))
-                    }.padding([.bottom, .horizontal], 3)
+                    }.padding([.bottom, .horizontal], 1)
                         .frame(maxWidth: 70)
 
                 }
@@ -90,54 +90,60 @@ extension IntroView {
     }
 
     private var ProfileChooserView: some View {
-        VStack(alignment: .center, spacing: 15) {
-            NavigationLink(destination: EnrollmentInfoView()) {
-                VStack(alignment: .center, spacing: 10) {
-                    Image("CustomerIntro")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding(10)
-                        .shadow(color: .lightGray.opacity(0.5), radius: 3)
-                    Text("Request")
-                        .foregroundColor(.black)
-                        .font(.system(size: 16, weight: .medium,
-                                      design: .rounded))
-                }.padding(.horizontal).padding().overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(Color.lightGray.opacity(0.5), lineWidth: 1)
-                        .frame(maxWidth: .infinity).shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
-                )
-            }
-            NavigationLink(destination: ProviderSearchView()) {
-                VStack(alignment: .center, spacing: 10) {
-                    Image("ProviderSearchIntro")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding(10)
-                        .shadow(color: .lightGray.opacity(0.5), radius: 3)
-                    Text("Provider search").foregroundColor(.black).font(.system(size: 16, weight: .medium, design: .rounded))
+        GeometryReader { geometry in
 
-                }.padding(.horizontal).padding()
-                    .overlay(RoundedRectangle(cornerRadius: 16)
+                VStack(alignment: .trailing, spacing: 15) {
+                    NavigationLink(destination: EnrollmentInfoView()) {
+                        VStack(alignment: .center, spacing: 10) {
+                            Image("CustomerIntro")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+								.padding([.horizontal, .top], 5)
+                            //                        .shadow(color: .lightGray.opacity(0.5), radius: 3)
+                            Text("Request")
+                                .foregroundColor(.black)
+                                .font(.system(size: 16, weight: .medium,
+                                              design: .rounded))
+                        }.padding(.bottom, 10).padding(.horizontal,25)
+							.overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(Color.lightGray.opacity(0.5), lineWidth: 1)
+                            //                            .frame(maxWidth: .infinity)//.shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
+                        )
+                    }
+                    NavigationLink(destination: ProviderSearchView()) {
+                        VStack(alignment: .center, spacing: 10) {
+                            Image("ProviderSearchIntro")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+								.padding([.horizontal, .top], 5)
+                            //                        .shadow(color: .lightGray.opacity(0.5), radius: 3)
+                            Text("Provider search").foregroundColor(.black).font(.system(size: 16, weight: .medium, design: .rounded)).lineLimit(1)
+                                .padding(.horizontal, -20)
+                        }.padding(.bottom, 10).padding(.horizontal,25)
+                            .overlay(RoundedRectangle(cornerRadius: 16)
                                 .stroke(address == "Enter new address" ? Color.lightGray.opacity(0.5) : Color.accentColor.opacity(0.6), lineWidth: 1)
-                                .frame(maxWidth: .infinity) .shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
-                    )
-            }
-            NavigationLink(destination: EnrollmentInfoView()) {
-                VStack(alignment: .center, spacing: 10) {
-                    Image("BecomeProviderIntro").resizable().aspectRatio(contentMode: .fit)
-                        .padding(10)
-                        .shadow(color: .lightGray.opacity(0.5), radius: 3)
-                    Text("Become to provider").foregroundColor(.black).font(.system(size: 15, weight: .medium, design: .rounded))
-                }.padding(.horizontal).padding().overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(Color.lightGray.opacity(0.5), lineWidth: 1)
-                        .frame(maxWidth: .infinity).shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
+                                     //                            .frame(maxWidth: .infinity) //.shadow(color: .secondary.opacity(0.6), radius: 3, y: 3)
+                            )
+                    }
+                    NavigationLink(destination: EnrollmentInfoView()) {
+                        VStack(alignment: .center, spacing: 10) {
+                            Image("BecomeProviderIntro")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+								.padding([.horizontal, .top], 5)
+                            //                        .shadow(color: .lightGray.opacity(0.5), radius: 3)
+                            Text("Become a provider").foregroundColor(.black).font(.system(size: 16, weight: .medium, design: .rounded)).padding(.horizontal, -20).lineLimit(1)
+						}.padding(.bottom, 10).padding(.horizontal,25)
+							.overlay(
+								RoundedRectangle(cornerRadius: 16)
+									.stroke(Color.lightGray.opacity(0.5), lineWidth: 1)
+                        )
+                    }
 
-                )
-            }
-
-        }.padding([.vertical]).padding([.bottom])
+				}.frame(maxWidth: geometry.size.width).padding([.top], 20)
+    
+        }.frame(maxWidth: .infinity)
     }
 }
 
